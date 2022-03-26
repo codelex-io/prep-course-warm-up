6099bc4a937c2c4de739025609e61f2ee0787d7e
Andis Kačs <mnemosx@gmail.com>
2022-03-26 23:56:45 +0200

    Update 29-draw-triangle.ts

--- src/29-draw-triangle.ts
+++ src/29-draw-triangle.ts
@@ -3,11 +3,6 @@ export {};
 /**
  * If you find yourself stuck with this exercise, perhaps a more visual approach would help:
  *  - https://blog.codeanalogies.com/2017/11/07/javascript-for-loops-explained/
- *
- * After you've finished this exercise (first of all, congrats! loops can be hard),
- * here are some more exercises similar to this one.
- *  - https://github.com/codelex-io/noob-exercises/tree/main/01-loops
- * Be warned - some of the exercises there can certainly be a tough nuts to crack.
  */
 
 function draw() {}
