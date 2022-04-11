7df4b29790ddaaf6a3179b4fe04d905d59e90d1e
wolfinj <wolfinj@gmail.com>
2022-04-11 21:33:03 +0300

    test changes

--- src/34-protect-email.ts
+++ src/34-protect-email.ts
@@ -6,3 +6,5 @@ export {};
 
 console.log(protectEmail("secret123@codelex.io")); // Expected result: sec...@codelex.io
 console.log(protectEmail("example@example.com")); // Expected result: exa...@example.com
+console.log(protectEmail('12345@example.com')); // Expected result: 12...@example.com
+console.log(protectEmail('12@example.com')); // Expected result: ...@example.com
